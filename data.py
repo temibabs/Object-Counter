e96efaa350af0d188eacab49187ba7375f3fbef6
Temi Babs <temi.ayo.babs@gmail.com>
2018-07-15 21:38:30 -0700

    Update data.py

--- data.py
+++ data.py
@@ -1,3 +1,19 @@
 from __future__ import division
 
 import tensorflow as tf
+import sys
+
+class Data(object):
+  def __init__(directory):
+    '''directory: The file directory where the training images are located.'''
+    self.dir = directory
+    if not tf.gfile.Exists(self.dir):
+      print('[!] The direcctory is non-existent.')
+      sys.exit()
+    if not tf.gfile.IsDirectory(self.dir):
+      print('[!] The selected file is not a directory. Please refer to a directory.')
+      sys.exit()
+    self.setup()
+    
+  def setup(self):
+    self.files = os.listdir(self.dir)
