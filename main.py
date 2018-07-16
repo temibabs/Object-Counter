efd91a9c4e4930551e67e2c5f0e9645a6c2ced91
Temi Babs <temi.ayo.babs@gmail.com>
2018-07-15 21:44:16 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 import tensorflow as tf
+from data import Data
+
 if tf.__version__ < '1.8.0':
   raise ImportError('Please upgrade your tensorflow installation to v1.8.* or later!')
+
+dir = 'dataset'
+data = Data(dir)
